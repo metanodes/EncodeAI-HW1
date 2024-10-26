72f2983324df7ff28c722de6b5e9058b5d17e40e
MetaNodes <meta.nodes@outlook.com>
2024-10-26 21:22:13 +0100

    Week 1 project initial commit

---
+++ HW1ChefGPT.py
@@ -0,0 +1,59 @@
+import os
+from openai import OpenAI
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are Gordan Ramsey, the British celebrity chef known for his fiery temper, aggressive behaviour, strict demeanour, making blunt, critical, and controversial comments",
+    }
+]
+messages.append(
+    {
+        "role": "system",
+        "content": "Your client is going to ask for a recipe about a specific dish. If you do not recognize the dish, you should not try to generate a recipe for it. Do not answer a recipe if you do not understand the name of the dish. If you know the dish, you must answer directly with a detailed recipe for it. If you don't know the dish, you should answer that you don't know the dish and end the conversation.",
+    }
+)
+
+dish = input("Type the name of the dish you want a recipe for:\n")
+messages.append(
+    {
+        "role": "user",
+        "content": f"Suggest me a detailed recipe and the preparation steps for making {dish}",
+    }
+)
+
+model = "gpt-4o-mini"
+
+stream = client.chat.completions.create(
+    model=model,
+    messages=messages,
+    stream=True,
+)
+
+collected_messages = []
+for chunk in stream:
+    chunk_message = chunk.choices[0].delta.content or ""
+    print(chunk_message, end="")
+    collected_messages.append(chunk_message)
+
+messages.append({"role": "system", "content": "".join(collected_messages)})
+
+while True:
+    print("\n")
+    user_input = input()
+    messages.append({"role": "user", "content": user_input})
+    stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+    collected_messages = []
+    for chunk in stream:
+        chunk_message = chunk.choices[0].delta.content or ""
+        print(chunk_message, end="")
+        collected_messages.append(chunk_message)
+
+    messages.append({"role": "system", "content": "".join(collected_messages)})
